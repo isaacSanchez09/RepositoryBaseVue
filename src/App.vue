64ff6feaa999cf672d66690683e3bbaa4d6ec22a
adrianpascual142003 <adrianpascual142003@gmail.com>
2023-01-17 16:43:52 +0100

    [ADD] Repository Base

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import { useNameStore } from './stores/name';
+import { mapActions } from 'pinia';
+
+
+
+  export default {
+    components: {
+
+},
+    methods: {
+      ...mapActions(useNameStore, {
+        loadData: 'loadData'
+      })
+    },
+    mounted() {
+      this.loadData()
+    }
+  }
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Configurado Por Defecto</h1>
+    <RouterView />
+  </div>
+</template>
---
+++ src/stores/name.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+import axios, { Axios } from 'axios';
+
+const SERVER = import.meta.env.VITE_SERVERURL;
+
+export const useNameStore = defineStore('counter', {
+  state() {
+    return {
+      
+    }
+  },
+
+  actions: {
+    loadData() {
+      
+    }
+  }
+})
